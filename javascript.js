d2267b38945b6c7f31dc9a7d9243fe020f7aa43c
SamRanddals <bubblerbig120@gmail.com>
2024-12-28 17:31:06 -0500

    se agregaron efectos especiales

--- javascript.js
+++ javascript.js
@@ -4,7 +4,9 @@
 
 let main = document.querySelector(".main-center");
 let button = document.querySelector(".checkbox");
+let audio = document.querySelector(".audio")
 
+let fondo= document.querySelector(".fondo")
 let isSansActive = false; // Bandera para alternar entre fondos
 
 button.addEventListener("click", function () {
@@ -13,9 +15,14 @@ button.addEventListener("click", function () {
         main.style.backgroundSize = "cover";
         main.style.backgroundPosition = "center"; 
         main.style.color = "#222"; 
+        fondo.style.background="url('images/2320e398125e34e287dd09eb6d36aaa6 (1).jpg')"
+        fondo.style.backgroundSize="cover"
+        audio.play()
+        main.style.boxShadow="12px 12px 12px #black;"
         isSansActive = true; // Actualiza la bandera
     } else {
         main.style.background = "#f5f5f5"; // Vuelve al fondo original
         isSansActive = false; // Resetea la bandera
+        
     }
 });
